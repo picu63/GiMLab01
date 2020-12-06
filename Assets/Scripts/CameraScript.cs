90bb2cc7de9a8ed6bd58fc6586d07335ecb55fd2
Piotr Olearczyk <picu63@gmail.com>
2020-12-06 20:03:29 +0100

    first commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementController : MonoBehaviour
+{
+    //private Rigidbody rigidbody;
+
+    public float speed = 500;
+    public int points = 0;
+    public int winPoints = 10;
+    public Vector3 startPosition;
+    private Text mainText;
+    private Text pointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainText = GameObject.FindGameObjectWithTag("MainText").GetComponent<Text>();
+        pointsText = GameObject.FindGameObjectWithTag("PointsText").GetComponent<Text>();
+        startPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            mainText.text = string.Empty;
+            pointsText.text = "0";
+            gameObject.transform.position = startPosition;
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+        GetComponent<Rigidbody>().AddForce(movement*speed*Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "PickUp")
+        {
+            collider.gameObject.SetActive(false);
+            points++;
+            pointsText.text = points.ToString();
+        }
+
+        if (points >= winPoints)
+        {
+            mainText.text = "You WIN\nPress Enter to start new game.";
+        }
+    }
+}
